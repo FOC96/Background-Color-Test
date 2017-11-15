563c56dbe5dee7621717117f1c2bba59864c6507
Fernando Ortiz Rico Celio <fernando_oc96@hotmail.com>
2017-11-14 19:43:11 -0700

    SQL database

---
+++ BACS383.sql
@@ -0,0 +1,51 @@
+# ************************************************************
+# Sequel Pro SQL dump
+# Version 4541
+#
+# http://www.sequelpro.com/
+# https://github.com/sequelpro/sequelpro
+#
+# Host: 127.0.0.1 (MySQL 5.6.35)
+# Database: bacs383
+# Generation Time: 2017-11-15 02:37:43 +0000
+# ************************************************************
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+# Dump of table test
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `test`;
+
+CREATE TABLE `test` (
+  `userID` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `firstName` varchar(30) DEFAULT '',
+  `gender` varchar(1) DEFAULT '',
+  `timestampTag` timestamp NULL DEFAULT NULL,
+  `timeArticle1` float DEFAULT NULL,
+  `answer1` tinyint(1) DEFAULT NULL,
+  `answer2` tinyint(1) DEFAULT NULL,
+  `timeArticle2` float DEFAULT NULL,
+  `answer3` tinyint(1) DEFAULT NULL,
+  `answer4` tinyint(1) DEFAULT NULL,
+  `preference` char(1) DEFAULT '',
+  PRIMARY KEY (`userID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
